328bd570da50f9cf75911a42293b282a8a7f9b0a
vikas batham <vikas batham>
2017-04-17 21:01:12 -0600

    updated testcase

---
+++ src/test/java/com/example/ConfigFileUsingPropertySourceTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {
+        "wordCount.caseSensitive=false",
+        "wordCount.words.skip[0]=the",
+        "wordCount.words.skip[1]=an",
+        "wordCount.words.skip[2]=a",
+})
+public class ConfigFileUsingPropertySourceTest {
+    @Autowired
+    private WordCount wordCount;
+
+    @Test
+    public void configTest(){
+
+        Assert.assertThat(wordCount.getCaseSensitive(), Matchers.equalTo(false));
+        List<String> attributeList = new ArrayList<>();
+        attributeList.add("the");
+        attributeList.add("an");
+        attributeList.add("a");
+        Assert.assertThat(wordCount.getWords().getSkip(), Matchers.equalTo(attributeList));
+        
+    }
+}
